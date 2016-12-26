e6a1db0d7daaff2821f02f8e5bcba75a388aed4b
sukute <sukute@microsoft.com>
2016-12-26 18:59:55 +0530

    New Changes to for Online retail

---
+++ world.data/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace world.data
+{
+    public class Invoice
+    {
+        public int InvoiceNo { get; set; }
+        public int Stockcode { get; set; }
+        public string Description { get; set; }
+        public int Quality { get; set; }
+        public int InvoiceDate { get; set; }
+        public float UnitPrice { get; set; }
+        public int CustomerId { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ worldpopulation/Controllers/OnlineRetailController.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using world.data;
+
+namespace worldpopulation.Controllers
+{
+    public class OnlineRetailController : Controller
+    {
+        // GET: OnlineRetail
+        public ActionResult Index()
+        {
+            var client = new RestClient();
+            client.AddHandler("application/json", new JsonDeserializer());
+            client.BaseUrl = new Uri(ConfigurationManager.AppSettings["ServiceURL"].ToString());
+            var request = new RestRequest("/api/OnlineRetail", Method.GET);
+            var response = client.Execute<List<Invoice>>(request);
+            return View("Index", response);
+        }
+    }
+}
