586c98322d91544c149cb826bb892c74e80f339e
sukute <sukute@microsoft.com>
2017-06-12 18:21:11 +0530

    Calling to API Added

--- worldpopulation.web/Controllers/HomeController.cs
+++ worldpopulation.web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +11,11 @@ namespace worldpopulation.Controllers
 {
     public class HomeController : Controller
     {
+        string authority = ConfigurationManager.AppSettings["authority"];
+        string clientId = ConfigurationManager.AppSettings["clientId"];
+        string clientSecret = ConfigurationManager.AppSettings["clientSecret"];
+        string resource = ConfigurationManager.AppSettings["resource"];
+        string serviceURL = ConfigurationManager.AppSettings["serviceURL"];
         public ActionResult Index()
         {
             return View();
@@ -16,7 +24,11 @@ namespace worldpopulation.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            var token = GetS2SAccessTokenAsync(authority, resource, clientId, clientSecret);
+            ViewBag.Token = token.AccessToken;
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync($"{resource}/api/values/5").Result;
+            ViewBag.Result = result.Content.ToString();
             return View();
         }
 
@@ -25,6 +37,15 @@ namespace worldpopulation.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private AuthenticationResult GetS2SAccessTokenAsync(string authority, string resource, string clientId, string clientSecret)
+        {
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+            AuthenticationContext context = new AuthenticationContext(authority);
+            var authenticationResult = context.AcquireTokenAsync(resource,
+                clientCredential).Result;
+            return authenticationResult;
         }
     }
 }
